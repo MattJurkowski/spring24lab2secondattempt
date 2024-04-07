1215a5802032939172592532205ac673e5dfde85
MattJurkowski <jurek198@gmail.com>
2024-04-07 12:16:36 +0000

    final lab2 commitment
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,16 +6,12 @@ int max_value = 5;
 
 int fib(int n){
     if(n == 0 || n == 1){
-        printf("*");
         return n;
     }
     else{
-        printf("(%d), (%d)", n-1, n-2);
         return(fib(n-1) + fib(n-2));
     }
 }
-
-
 // main() runs in its own thread in the OS
 int main()
 {
@@ -23,12 +19,9 @@ int main()
     printf("series is: ");
     for (int i=0; i<max_value; i++){
         printf("%d, \r\n ", fib(i));
-
     }
     printf("\r\n");
-
     while (true) {
-
     }
 }
 
